7daa0d5ae3d9b4aed040029ca3a9f348fbf37c51
maqiduo <bjfriend009@gmail.com>
2020-10-30 18:35:22 +0800

    update

--- panel.go
+++ panel.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/robfig/cron"
+
 	"github.com/shirou/gopsutil/load"
 	"google.golang.org/grpc"
 	"time"
 	"v2ray.com/core/common/protocol"
 	"v2ray.com/core/common/serial"
 	"v2ray.com/core/proxy/vmess"
+
+	"github.com/Uhtred009/ssrpanel-plugin/api/client"
 )
 
 type Panel struct {
